41ce3af355070c50a28dbdb6a07bac40798e44cc
Alex Linden Levy <lindenle@uiuc.edu>
2011-12-28 14:02:02 -0800

    more docs for the exceptions

--- ManagerContainerExceptions.hpp
+++ ManagerContainerExceptions.hpp
@@ -3,18 +3,26 @@
 
 #include <exception>
 
+/// ManagerNotExists is an exception to throw when a manager does
+/// exist in the ManagerContainer map but shouldn't.
+
 class ManagerExists: public exception
 {
+  /// describes the error that was encountered
   virtual const char* what() const throw()
   {
     return "Manager exists";
   }
 };
 
+/// ManagerNotExists is an exception to throw when a manager does not
+/// exist in the ManagerContainer map but should.
+
 class ManagerNotExists: public exception
 {
   virtual const char* what() const throw()
   {
+    /// describes the error that was encountered
     return "Manager does not exist";
   }
 };
