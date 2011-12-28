a3f0550981604ece1644e9b490b86627351fbaec
Alex Linden Levy <lindenle@uiuc.edu>
2011-12-28 13:36:04 -0800

    finalizing the documentation

--- Client.hpp
+++ Client.hpp
@@ -6,19 +6,28 @@
 
 using namespace std;
 
+/// Client class example. The client is required to have one member
+/// function that takes a boolean and a string. This slot function can
+/// be connected to a managers update signal through the
+/// ManagerContainer or through the Manager directly.
+
 class Client
 {
 
 public:
-  
+
+  /// constructor with one parameter, the name of the client.
   Client(string name = "default" );
   
+  /// destructor
   ~Client(){;}
-  
+
+  /// slot for signal connection
   void manager_update(bool selected, string name);
   
 private:
 
+  /// client name
   string _name;
 
 };
